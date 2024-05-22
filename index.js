798d2f701896b57248f4e158d1a845db406feccd
Meng <mxiong600@gmail.com>
2024-05-22 18:58:54 -0500

    daily commit

--- index.js
+++ index.js
@@ -1,24 +1,58 @@
+let isModalOpen = false
+let contrastToggle = false
+const scaleFactor = 1 / 20
 
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape")
+    const x = event.clientX * scaleFactor
+    const y = event.clientY * scaleFactor
 
-function contact(event) {
-    event.preventDefault()
-   // emailjs
-   //     .sendForm(
-    //        'service_44rzn2k',
-    //        'template_470wblg',
-    //        event.target,
-    //        'shLES4t1afgFHflyp'
-     //   ).then(() => {
-     //       console.log(`this worked`)
-     //   })
-
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0
+        const boollInt = isOdd ? -1 : 1
+        shapes[i].style.transform = `translate(${x * boollInt}px,${y * boollInt}px)`
+    }
 }
 
-const loading = document.querySelector( '.modal__overlay--loading')
-const success = document.querySelector( '.modal__overlay--success')
-loading.classList += " modal__overlay--visible"
+function toggleContrast() {
+    contrastToggle = !contrastToggle
+    if(contrastToggle) {
+        document.body.classList += "dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
 
-setTimeout(() => {
- loading.classList.remove('modal__overlay--visible');
- success.classList += " modal__overlay--visible"
-}, 1000);
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " modal__overlay--visible"
+    emailjs
+        .sendForm(
+            'service_44rzn2k',
+            'template_470wblg',
+            event.target,
+            'shLES4t1afgFHflyp'
+        ).then(() => {
+            loading.classList.remove("modal__overlay--visible")
+            success.classList += " modal__overlay--visible"
+        }).catch(() => {
+            loading.catch.remove("modal__overlay--visible");
+            alert(
+                "The email service is temporarily unavailable. Please contact me directly @mxiong600@gmail.com."
+            )
+        })
+
+}
+ 
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true
+    document.body.classList += " modal--open"
+}
+
